4a103fef538ff9c83c7c664f4ecd742bd09db46f
mustafa <mustafa01ali@gmail.com>
2011-09-23 15:57:33 +0530

    Added licensing information

--- src/com/example/countrycountacts/ContactDetailsActivity.java
+++ src/com/example/countrycountacts/ContactDetailsActivity.java
@@ -1,6 +1,22 @@
 /**
+ * Copyright (C) 2011, Mir Mustafa Ali (mustafa01ali@gmail.com)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * 
  */
+
 package com.example.countrycountacts;
 
 import android.app.Activity;
@@ -43,7 +59,6 @@ public class ContactDetailsActivity extends Activity {
         
         ListView lv = (ListView) findViewById(R.id.numbers_list);
         mPhoneNumbers = contact.getPhoneNumbers();
-        //System.out.println("Numbers: " + mPhoneNumbers.size());
         
         lv.setAdapter(new ArrayAdapter<Object>(this, android.R.layout.simple_list_item_1, mPhoneNumbers.toArray()));
         //lv.setAdapter(new ArrayAdapter<Object>(this, R.layout.phone_field, R.id.number, phoneNumbers.toArray()));
--- src/com/example/countrycountacts/HomeActivity.java
+++ src/com/example/countrycountacts/HomeActivity.java
@@ -1,160 +1,176 @@
 /**
+ * Copyright (C) 2011, Mir Mustafa Ali (mustafa01ali@gmail.com)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * 
  */
+
 package com.example.countrycountacts;
 
-import android.app.Activity;
-import android.content.ContentResolver;
-import android.content.Intent;
-import android.database.Cursor;
-import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.provider.ContactsContract.CommonDataKinds.Phone;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Spinner;
+ import android.app.Activity;
+ import android.content.ContentResolver;
+ import android.content.Intent;
+ import android.database.Cursor;
+ import android.os.Bundle;
+ import android.provider.ContactsContract;
+ import android.provider.ContactsContract.CommonDataKinds.Phone;
+ import android.view.View;
+ import android.widget.AdapterView;
+ import android.widget.AdapterView.OnItemClickListener;
+ import android.widget.AdapterView.OnItemSelectedListener;
+ import android.widget.ArrayAdapter;
+ import android.widget.ListView;
+ import android.widget.Spinner;
 
-import java.util.ArrayList;
+ import java.util.ArrayList;
 
-/**
- * Home Activity
- * @author mustafa.ali
- */
-public class HomeActivity extends Activity {
+ /**
+  * Home Activity
+  * @author mustafa.ali
+  */
+ public class HomeActivity extends Activity {
 
-    private Spinner mSpinner;
-    private ListView mContactsList;
-    private String[] mCodes;
-    private ArrayList<String> mContactNames;
-    private ArrayList<Contact> mContacts;
+     private Spinner mSpinner;
+     private ListView mContactsList;
+     private String[] mCodes;
+     private ArrayList<String> mContactNames;
+     private ArrayList<Contact> mContacts;
 
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+     /** Called when the activity is first created. */
+     @Override
+     public void onCreate(Bundle savedInstanceState) {
+         super.onCreate(savedInstanceState);
+         setContentView(R.layout.main);
 
-        init();
-    }
+         init();
+     }
 
-    /**
-     * Initializes the views, fields
-     */
-    private void init(){
-        mSpinner = (Spinner) findViewById(R.id.spinner1);
-        mContactsList = (ListView) findViewById(R.id.listView1);
-        
-        mContacts = new ArrayList<Contact>();
-        mContactNames = new ArrayList<String>();
-        
-        mCodes = getResources().getStringArray(R.array.codes);
+     /**
+      * Initializes the views, fields
+      */
+     private void init(){
+         mSpinner = (Spinner) findViewById(R.id.spinner1);
+         mContactsList = (ListView) findViewById(R.id.listView1);
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
-                this, R.array.countries, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mSpinner.setAdapter(adapter);
+         mContacts = new ArrayList<Contact>();
+         mContactNames = new ArrayList<String>();
 
-        mSpinner.setOnItemSelectedListener(new CountrySelectedListener());
+         mCodes = getResources().getStringArray(R.array.codes);
 
-        mContactsList.setOnItemClickListener(new OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View view,
-                    int position, long id) {
-                Intent intent = new Intent(HomeActivity.this, ContactDetailsActivity.class);
-                intent.putExtra("contact", mContacts.get(position));
-                startActivity(intent);
-            }
-        });
+         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                 this, R.array.countries, android.R.layout.simple_spinner_item);
+         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+         mSpinner.setAdapter(adapter);
 
-    }
+         mSpinner.setOnItemSelectedListener(new CountrySelectedListener());
 
-    /**
-     * Class to provide a callback method that will notify when an item has been selected from the Spinner
-     * 
-     * @author mustafa.ali
-     *
-     */
-    public class CountrySelectedListener implements OnItemSelectedListener {
+         mContactsList.setOnItemClickListener(new OnItemClickListener() {
+             public void onItemClick(AdapterView<?> parent, View view,
+                     int position, long id) {
+                 Intent intent = new Intent(HomeActivity.this, ContactDetailsActivity.class);
+                 intent.putExtra("contact", mContacts.get(position));
+                 startActivity(intent);
+             }
+         });
 
-        public void onItemSelected(AdapterView<?> parent,
-                View view, int pos, long id) {
-            
-            mContactNames.clear();
-            mContacts.clear();
-            getContactsByCountryCode(mCodes[pos]);
-            setArrayAdapterToListView();
-        }
+     }
 
-        public void onNothingSelected(AdapterView<?> parent) {
-            // Do nothing.
-        }
-    }
+     /**
+      * Class to provide a callback method that will notify when an item has been selected from the Spinner
+      * 
+      * @author mustafa.ali
+      *
+      */
+     public class CountrySelectedListener implements OnItemSelectedListener {
 
-    /**
-     * Fetches contacts from the phone DB as per the country code
-     * @param code Country + prefix code
-     */
-    private void getContactsByCountryCode(String code){
-        
-        ContentResolver cr = getContentResolver();
-        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
-                null, null, null, ContactsContract.Contacts.DISPLAY_NAME);
-        
-        if (cur.getCount() > 0) {
-            while (cur.moveToNext()) {
-                
-                String id = cur.getString(
-                        cur.getColumnIndex(ContactsContract.Contacts._ID));
-                String name = cur.getString(
-                        cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-                
-                if (Integer.parseInt(cur.getString(
-                        cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
+         public void onItemSelected(AdapterView<?> parent,
+                 View view, int pos, long id) {
 
-                    Contact contact = new Contact(name);
-                    
+             mContactNames.clear();
+             mContacts.clear();
+             getContactsByCountryCode(mCodes[pos]);
+             setArrayAdapterToListView();
+         }
 
-                    Cursor pCur = cr.query(
-                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI, 
-                            null, 
-                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = ?", 
-                            new String[]{id}, null);
-                    
-                    int phoneColumnIndex = pCur.getColumnIndex(Phone.NUMBER);
-                    String phoneNumber = "";
-                    
-                    while (pCur.moveToNext()) {
-                        
-                        phoneNumber = pCur.getString(phoneColumnIndex);
-                        
-                        if (phoneNumber.startsWith(code)) {
-                            contact.addPhoneNumber(pCur.getString(phoneColumnIndex));    
-                        }
-                        
-                    } 
-                    
-                    if (contact.hasPhoneNumbers()) {
-                        mContactNames.add(name);
-                        mContacts.add(contact);                        
-                    }
-                    
-                    pCur.close();
-                }
-            }
-        }
-        cur.close();
-    }
+         public void onNothingSelected(AdapterView<?> parent) {
+             // Do nothing.
+         }
+     }
 
-    /**
-     * Sets the cursor adapter to the list view
-     * @param c Database cursor
-     */
-    private void setArrayAdapterToListView(){
-        mContactsList.setAdapter(new ArrayAdapter<Object>(this, android.R.layout.simple_list_item_1, mContactNames.toArray()));
-        //mContactsList.setAdapter(new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, c, new String[] { Contacts.DISPLAY_NAME }, new int[] { android.R.id.text1 }));
-    }
+     /**
+      * Fetches contacts from the phone DB as per the country code
+      * @param code Country + prefix code
+      */
+     private void getContactsByCountryCode(String code){
 
-}
+         ContentResolver cr = getContentResolver();
+         Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
+                 null, null, null, ContactsContract.Contacts.DISPLAY_NAME);
+
+         if (cur.getCount() > 0) {
+             while (cur.moveToNext()) {
+
+                 String id = cur.getString(
+                         cur.getColumnIndex(ContactsContract.Contacts._ID));
+                 String name = cur.getString(
+                         cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+
+                 if (Integer.parseInt(cur.getString(
+                         cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
+
+                     Contact contact = new Contact(name);
+
+
+                     Cursor pCur = cr.query(
+                             ContactsContract.CommonDataKinds.Phone.CONTENT_URI, 
+                             null, 
+                             ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = ?", 
+                                     new String[]{id}, null);
+
+                     int phoneColumnIndex = pCur.getColumnIndex(Phone.NUMBER);
+                     String phoneNumber = "";
+
+                     while (pCur.moveToNext()) {
+
+                         phoneNumber = pCur.getString(phoneColumnIndex);
+
+                         if (phoneNumber.startsWith(code)) {
+                             contact.addPhoneNumber(pCur.getString(phoneColumnIndex));    
+                         }
+
+                     } 
+
+                     if (contact.hasPhoneNumbers()) {
+                         mContactNames.add(name);
+                         mContacts.add(contact);                        
+                     }
+
+                     pCur.close();
+                 }
+             }
+         }
+         cur.close();
+     }
+
+     /**
+      * Sets the cursor adapter to the list view
+      * @param c Database cursor
+      */
+     private void setArrayAdapterToListView(){
+         mContactsList.setAdapter(new ArrayAdapter<Object>(this, android.R.layout.simple_list_item_1, mContactNames.toArray()));
+         //mContactsList.setAdapter(new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, c, new String[] { Contacts.DISPLAY_NAME }, new int[] { android.R.id.text1 }));
+     }
+
+ }
